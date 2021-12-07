56eefc2078ddf811ce351763c8ab8b2a4994c67d
algokelvin-373 <kelvinht373@gmail.com>
2021-12-07 21:19:55 +0700

    [Android] [VisitorApps] Set Data Visitor

---
+++ Android/VisitorApps/app/src/main/java/com/algokelvin/visitorapps/DataSample.java
@@ -0,0 +1,50 @@
+package com.algokelvin.visitorapps;
+
+import java.util.ArrayList;
+
+public class DataSample {
+    public static ArrayList<Visitor> setData() {
+        ArrayList<Visitor> data = new ArrayList<>();
+        data.add(new Visitor(1, "10 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(2, "10 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(3, "10 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(4, "10 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(5, "10 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(6, "08 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(7, "08 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(8, "08 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(9, "07 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(10, "07 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(11, "07 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        data.add(new Visitor(12, "04 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(13, "04 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(14, "03 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(15, "03 Desember 2022", "Fransisco",
+                "Jl. Meteng Kalibata, Jakarta Barat"));
+        data.add(new Visitor(16, "03 Desember 2022", "Yulia Grace",
+                "Tangerang, Banten"));
+        data.add(new Visitor(17, "03 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(18, "02 Desember 2022", "Kelvin Herwanda Tandrio",
+                "Jl. Merak No.51 Jaten, Jawa Tengah"));
+        data.add(new Visitor(19, "01 Desember 2022", "Veren Valencia",
+                "Solo Creator, Tangerang Selatan"));
+        data.add(new Visitor(20, "01 Desember 2022", "Thea B",
+                "Jl. Kemayoran, Jakarta Utara, DKI Jakarta"));
+        return data;
+    }
+}
--- Android/VisitorApps/app/src/main/java/com/algokelvin/visitorapps/MainActivity.kt
+++ Android/VisitorApps/app/src/main/java/com/algokelvin/visitorapps/MainActivity.kt
@@ -2,10 +2,32 @@ package com.algokelvin.visitorapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.algokelvin.visitorapps.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.item_visitor.view.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            setRecyclerView()
+        }
+
     }
+
+    private fun ActivityMainBinding.setRecyclerView() {
+        val visitor = DataSample.setData()
+        this.rvItem.layoutManager = LinearLayoutManager(this@MainActivity)
+        this.rvItem.adapter = DataAdapter(visitor.size, R.layout.item_visitor) { view, position ->
+            view.date_visitor.text = visitor[position].date
+            view.name_visitor.text = ("Name: ${visitor[position].name}")
+            view.address_visitor.text = visitor[position].address
+        }
+    }
+
 }
