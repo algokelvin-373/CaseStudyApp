5b0dba1110186228f9d2e602fdf792225c810f02
algokelvin-373 <kelvinht373@gmail.com>
2021-12-08 14:28:09 +0700

    [Android] [VisitorApps] 'Create BorderUI' , 'Create Function getDpSize', and 'Success Set Border and Date Data Structure'

--- Android/VisitorApps/app/src/main/java/com/algokelvin/visitorapps/MainActivity.kt
+++ Android/VisitorApps/app/src/main/java/com/algokelvin/visitorapps/MainActivity.kt
@@ -2,12 +2,14 @@ package com.algokelvin.visitorapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.algokelvin.visitorapps.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.item_visitor.view.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var header = "null"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +24,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun ActivityMainBinding.setRecyclerView() {
         val visitor = DataSample.setData()
+        val borderUI = BorderUI(this@MainActivity, color = R.color.white,
+            colorStroke = R.color.black, sizeStroke = 2, radius = ConstFunc.getSizeDp(resources, 8))
+
         this.rvItem.layoutManager = LinearLayoutManager(this@MainActivity)
         this.rvItem.adapter = DataAdapter(visitor.size, R.layout.item_visitor) { view, position ->
-            view.date_visitor.text = visitor[position].date
+            if (visitor[position].date != header) {
+                view.date_visitor.text = visitor[position].date
+                header = visitor[position].date
+            } else {
+                view.date_visitor.visibility = View.GONE
+            }
+
+            view.cl_data_visitor.background = borderUI.getBorder()
             view.name_visitor.text = ("Name: ${visitor[position].name}")
             view.address_visitor.text = visitor[position].address
         }
